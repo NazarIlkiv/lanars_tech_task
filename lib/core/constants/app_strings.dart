970b159db14a258b8319c9a8f78e12bc10942a51
NazarIlkiv <bruem35@gmail.com>
2024-12-04 17:16:51 +0200

    add strings for autorization feature

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,8 @@
+class AppStrings {
+  static const signIn = 'Sign in';
+  static const email = 'Email';
+  static const enterYourEmail = 'Enter your email';
+  static const password = 'Password';
+  static const enterYourPassword = 'Enter your password';
+  static const logIn = 'Log in';
+}
