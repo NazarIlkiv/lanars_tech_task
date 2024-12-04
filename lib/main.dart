bc8c5f718c46e76e69164eeaa4d17796feb2d318
NazarIlkiv <bruem35@gmail.com>
2024-12-04 17:17:29 +0200

    add blocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:lanars_tech_task/constants/app_strings.dart';
-import 'package:lanars_tech_task/theme/app_text_styles.dart';
-import 'package:lanars_tech_task/theme/theme.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:lanars_tech_task/core/theme/theme.dart';
+import 'package:lanars_tech_task/core/utils/di/injection.dart';
+import 'package:lanars_tech_task/presentation/state/auth/auth_cubit.dart';
+import 'package:lanars_tech_task/presentation/state/user/user_cubit.dart';
+import 'package:lanars_tech_task/presentation/view/screens/main_screen/main_screen.dart';
+import 'package:lanars_tech_task/presentation/view/screens/sign_in_screen/sign_in_screen.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getTemporaryDirectory(),
+  );
+  configureDependencies();
+
   runApp(const MainApp());
 }
 
@@ -12,15 +25,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            AppStrings.helloWorld,
-            style: AppTextStyles.titleText,
-            ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthCubit>(
+          create: (_) => getIt<AuthCubit>(),
+        ),
+        BlocProvider<UserCubit>(
+          create: (_) => getIt<UserCubit>(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        home: BlocBuilder<AuthCubit, AuthState>(
+          builder: (context, state) {
+            return state.isLoggedIn ? const MainScreen() : const SignInScreen();
+          },
         ),
       ),
     );
