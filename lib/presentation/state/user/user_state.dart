f9e8da0a0465bbcad798faa009f2350df38aa2cc
NazarIlkiv <bruem35@gmail.com>
2024-12-05 00:33:11 +0200

    improve code

--- lib/presentation/state/user/user_state.dart
+++ lib/presentation/state/user/user_state.dart
@@ -1,19 +1,30 @@
 part of 'user_cubit.dart';
 
-abstract class UserState {}
+abstract class UserState extends Equatable {
+  const UserState();
+
+  @override
+  List<Object?> get props => [];
+}
 
 class UserLoadingState extends UserState {}
 
 class UserLoadedState extends UserState {
   final UserModel user;
 
-  UserLoadedState(this.user);
+  const UserLoadedState(this.user);
+
+  @override
+  List<Object?> get props => [user];
 }
 
 class UserErrorState extends UserState {
   final String errorMessage;
 
-  UserErrorState(this.errorMessage);
+  const UserErrorState(this.errorMessage);
+
+  @override
+  List<Object?> get props => [errorMessage];
 }
 
 class UserEmptyState extends UserState {}
