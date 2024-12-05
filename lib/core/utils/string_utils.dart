b6be1a076df93e0ce67ffe632a25356a6370fe86
NazarIlkiv <bruem35@gmail.com>
2024-12-05 11:05:45 +0200

    create file for string utils

---
+++ lib/core/utils/string_utils.dart
@@ -0,0 +1,8 @@
+class StringUtils {
+  String truncateString(String text, int maxLength) {
+    if (text.length > maxLength) {
+      return '${text.substring(0, maxLength)}...';
+    }
+    return text;
+  }
+}
