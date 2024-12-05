59919872757fe640ac2dd66eb111defe9e0ce3cc
NazarIlkiv <bruem35@gmail.com>
2024-12-05 11:06:02 +0200

    add new repository and cubit to injection container

--- lib/core/utils/di/injection_container.dart
+++ lib/core/utils/di/injection_container.dart
@@ -1,8 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:lanars_tech_task/data/network/dio_config.dart';
-import 'package:lanars_tech_task/data/repositories/repository_impl/user_repository_impl.dart';
+import 'package:lanars_tech_task/data/repositories/repository_impl/photos_list_repository/photo_list_repository_impl.dart';
+import 'package:lanars_tech_task/data/repositories/repository_impl/user_repository/user_repository_impl.dart';
 import 'package:lanars_tech_task/presentation/state/auth/auth_cubit.dart';
+import 'package:lanars_tech_task/presentation/state/photosList/photo_list_cubit.dart';
 import 'package:lanars_tech_task/presentation/state/user/user_cubit.dart';
 
 final getIt = GetIt.instance;
@@ -13,10 +15,16 @@ void injectionContainer() {
     ..registerSingleton<UserRepository>(
       UserRepository(getIt<Dio>()),
     )
+    ..registerSingleton<PhotoListRepository>(
+      PhotoListRepository(getIt<Dio>()),
+    )
     ..registerSingleton<AuthCubit>(
       AuthCubit(),
     )
     ..registerSingleton<UserCubit>(
       UserCubit(getIt<UserRepository>()),
+    )
+    ..registerSingleton<PhotoListCubit>(
+      PhotoListCubit(getIt<PhotoListRepository>()),
     );
 }
